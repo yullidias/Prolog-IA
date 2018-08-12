7fc418ba5a36e9ca082006887f2cf765381a0f4c
yullidias <yulli.dias@hotmail.com>
2018-08-11 23:13:48 -0300

    exercicio finalizado

--- Lista2/exercicio3.pl
+++ Lista2/exercicio3.pl
@@ -1,16 +1,18 @@
-casal(jose,maria).
-casal(maria,jose).
 progenitor(jose,joao).
 progenitor(jose,ana).
 progenitor(maria,joao).
 progenitor(maria,ana).
 progenitor(ana,helena).
 progenitor(ana,joana).
-progenior(joao,mario).
-progenior(helena,carlos).
-progenior(mario,carlos).
-prole(Z):-progenitor(X,Z).
-irma(X):- progenitor(Z,X), progenitor(Z,Y), X!=Y;
-avo(X):- progenitor(M,X),progenitor(E,M).
-tia(X):- progenitor(Y,X), irma(Y, Z).
-prima(X):-prole(tia(X)).
+progenitor(ana,marcos).
+progenitor(joao,mario).
+progenitor(helena,carlos).
+progenitor(mario,carlos).
+prole(P,F):-progenitor(P,F).
+irmaos(X,Y):-progenitor(Z,X),!, progenitor(Z,Y),X\==Y,!.
+descendente(X,Y):-progenitor(Y,X).
+descendente(X,Y):-progenitor(Z,X), descendente(Z,X).
+avo(X,Y):- progenitor(X,Z), progenitor(Z,Y).
+tia(T,S):-progenitor(Y,S), irmaos(Y,T).
+prima(X,Y):- progenitor(A,X), progenitor(B,Y), irmaos(A,B).
+
